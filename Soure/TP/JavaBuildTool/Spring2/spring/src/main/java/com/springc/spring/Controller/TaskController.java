a38f613f8aa03c70379404004ff6cad4bf4b0277
Sokmeak <saroussokmeak721@gmail.com>
2024-11-12 14:27:09 +0700

    move to windows

--- Soure/TP/JavaBuildTool/Spring2/spring/src/main/java/com/springc/spring/Controller/TaskController.java
+++ Soure/TP/JavaBuildTool/Spring2/spring/src/main/java/com/springc/spring/Controller/TaskController.java
@@ -4,6 +4,8 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
 
 @Controller
 @RequestMapping("/tp04")
@@ -17,9 +19,23 @@ public class TaskController {
     }
 
     @GetMapping("task2")
-    public String loginForm(Model model) {
+    public String signInForm(Model model) {
         
         return "task2";
     }
+
+
+    @GetMapping("task3")
+    public String signUpForm(Model model) {
+        
+        return "task3";
+    }
+
+ @GetMapping("test")
+ public String Test(Model model) {
+     return "test";
+ }
+ 
+
 
 }
