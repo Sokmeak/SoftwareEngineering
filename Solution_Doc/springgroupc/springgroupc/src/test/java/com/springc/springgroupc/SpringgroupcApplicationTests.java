6f1aca08cde485f769be9ba8ea62e02f3d0effd9
SARENSokmeak <saroussokmeak721@gmail.com>
2024-11-13 11:54:56 +0700

    Tp 04 and Tp05 from window

---
+++ Solution_Doc/springgroupc/springgroupc/src/test/java/com/springc/springgroupc/SpringgroupcApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springc.springgroupc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringgroupcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
---
+++ Soure/TP/JavaBuildTool/springgroupc/springgroupc/src/main/java/com/springc/springgroupc/Product/ProductRepository.java
@@ -0,0 +1,7 @@
+package com.springc.springgroupc.Product;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ProductRepository extends JpaRepository<ProductEntity, Long>{
+    
+}
